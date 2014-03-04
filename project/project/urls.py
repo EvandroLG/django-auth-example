d9539f3c94146f473e12ca5ec912ce8b894aec2f
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-03-04 15:45:29 -0300

    created base template

--- project/project/urls.py
+++ project/project/urls.py
@@ -5,13 +5,4 @@ admin.autodiscover()
 urlpatterns = patterns('',
   url(r'^users/', include('users.urls')),
   url(r'^admin/', include(admin.site.urls)),
-  # Examples:
-  # url(r'^$', 'project.views.home', name='home'),
-  # url(r'^project/', include('project.foo.urls')),
-
-  # Uncomment the admin/doc line below to enable admin documentation:
-  # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-  # Uncomment the next line to enable the admin:
-  # url(r'^admin/', include(admin.site.urls)),
 )
