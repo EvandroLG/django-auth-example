0ec81ae95f641d8920bc311d8692527df90389e4
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-03-04 15:43:06 -0300

    changed routes

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-  url(r'^$', 'project.views.users', name='home'),
+  url(r'^users/', include('users.urls')),
+  url(r'^admin/', include(admin.site.urls)),
   # Examples:
   # url(r'^$', 'project.views.home', name='home'),
   # url(r'^project/', include('project.foo.urls')),
